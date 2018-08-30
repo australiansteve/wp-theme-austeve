024458768ad863e4a73345ca51e91824f5a50bbd
Steve <steve@australiansteve.com>
2018-08-30 20:59:58 -0300

    Rip out Customizer support in base parent theme - it's too much maintenance and easier to custom build each site

--- header.php
+++ header.php
@@ -27,68 +27,30 @@ if ( file_exists( $svg_sprite ) ) {
 	echo $svg_sprite;
 } 
 
-$fixedBackground = get_theme_mod('austeve_background_fixed', 'fixed');
-$backgrounds = get_theme_mod('austeve_backgrounds', 0);
-$onlyforhome = get_theme_mod('austeve_background_homeonly', false);
 $pageClasses = is_home() ? "homepage" : "";
-
-if ($fixedBackground == 'fixed') 
-{
-	//Fixed
 ?>
-	<div id="background-div" class="fixed">
-<?php
-		for ($b = 0; $b < $backgrounds; $b++) {
-			if (!$onlyforhome || is_home())
-			{
-				echo '<div id="bgImage'.($b+1).'" class="bgImage">&nbsp;</div>';
-			}
-		}
-?>
-	</div>
 	
 	<div id="page" class="<?php echo $pageClasses; ?>">
 
-<?php 
-}
-else
-{
-	//Scrolling
-?>
-	<div id="background-div" class="scrolling">
-<?php
-		for ($b = 0; $b < $backgrounds; $b++) {
-			if (!$onlyforhome || is_home())
-			{
-				echo '<div id="bgImage'.($b+1).'" class="bgImage">&nbsp;</div>';
-			}	
-		}
-?>
+		<header id="masthead">
+			<section class="row column">
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+					</a>
+				</h1>
+				<h2 class="site-description"><?php echo esc_html( get_bloginfo( 'description' ) ); ?></h2>
+			</section>
 
-		<div id="page" class="<?php echo $pageClasses; ?>">
-<?php
-}
-?>
-
-<header id="masthead">
-	<section class="row column">
-		<h1 class="site-title">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-			</a>
-		</h1>
-		<h2 class="site-description"><?php echo esc_html( get_bloginfo( 'description' ) ); ?></h2>
-	</section>
-
-	<ul class="dropdown menu" data-dropdown-menu>
-		<?php
-		$args = [
-			'theme_location' 	=> 'primary',
-			'container'			=> false,
-			'items_wrap' 		=> '%3$s',
-			'walker' 			=> new AUSteve_Foundation_Dropdown_Nav_Menu(),
-		];
-		wp_nav_menu( $args ); ?>
-	</ul>
-</header>
-<div id="content" class="site-content" role="main">
+			<ul class="dropdown menu" data-dropdown-menu>
+				<?php
+				$args = [
+					'theme_location' 	=> 'primary',
+					'container'			=> false,
+					'items_wrap' 		=> '%3$s',
+					'walker' 			=> new AUSteve_Foundation_Dropdown_Nav_Menu(),
+				];
+				wp_nav_menu( $args ); ?>
+			</ul>
+		</header>
+		<div id="content" class="site-content" role="main">
